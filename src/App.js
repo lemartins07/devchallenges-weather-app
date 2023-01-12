c5136e7e65ecf5eb64e1e1f6e96c48f370ae2694
Leandro Martins <leandro.martins.85@gmail.com>
2023-01-11 22:30:51 -0300

    Initial geolocation and fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useEffect } from 'react'
+import useFetch from './hooks/useFetch'
+import { SEARCH_CIT_GET } from './services/api'
+
 import './styles/globals.css'
 import SideBar from './components/SideBar/index'
 import Main from './components/Main/index'
 
 function App() {
+  const { request } = useFetch()
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = SEARCH_CIT_GET()
+      const { json } = await request(url, options)
+      console.log(json)
+    }
+
+    getData()
+  }, [request])
+
+  const successCallback = (position) => {
+    console.log(position)
+  }
+
+  const errorCallback = (error) => {
+    console.log(error)
+  }
+
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
+  // console.log(data)
+
   return (
     <>
       <div className="wrapper">
--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,14 @@
 // export const API_URL = 'https://www.metaweather.com/api'
 const API_KEY = '87b2487435c1c3f65c0b8cc14ad2a3e9'
-const API_URL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`
+// const API_KEY = '43d2d1023ae5c6c8a741593c7261b46f'
+const portoAlegre = {
+  lat: '-30.033056',
+  lon: '-51.230000',
+}
+// https://api.openweathermap.org/data/2.5/weather?lat=-30.033056&lon=-51.230000&appid=43d2d1023ae5c6c8a741593c7261b46f&units=metric
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${portoAlegre.lat}&lon=${portoAlegre.lon}&appid=${API_KEY}&units=metric`
 
-// const CROSS_DOMAIN_TEST = 'https://cors-anywhere.herokuapp.com'
-
-// const REQUEST_URL = `${CROSS_DOMAIN_TEST}/${API_URL}`
-
-export function SEARCH_CIT_GET(name) {
+export function SEARCH_CIT_GET() {
   return {
     url: `${API_URL}`,
     options: {
